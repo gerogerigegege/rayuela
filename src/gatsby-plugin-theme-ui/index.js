c665d311424f3154f9edb90c4830023896963a35
Rodrigo Moreira <hola@piducan.dev>
2021-04-29 18:07:55 -0400

    style a little

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -3,12 +3,11 @@ const theme = {
   breakpoints: ["40em", "52em", "64em"],
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fonts: {
-    body:
-      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", sans-serif',
+    body: "system-ui, -apple-system, sans-serif",
     heading: "inherit",
     monospace: "Menlo, monospace",
   },
-  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 96],
+  fontSizes: [16, 20, 24, 32, 48, 64, 96, 128],
   fontWeights: {
     body: 400,
     heading: 700,
@@ -24,6 +23,9 @@ const theme = {
     primary: "#07c",
     secondary: "#30c",
     muted: "#f6f6f6",
+  },
+  sizes: {
+    container: 640,
   },
   text: {
     heading: {
@@ -61,6 +63,9 @@ const theme = {
     h6: {
       variant: "text.heading",
       fontSize: 0,
+    },
+    p: {
+      textAlign: "justify",
     },
     pre: {
       fontFamily: "monospace",
