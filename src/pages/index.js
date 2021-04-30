c14c7e02e9e70920b369a049ca569141e8ac0fd8
Rodrigo Moreira <hola@piducan.dev>
2021-04-30 14:54:07 -0400

    create sample pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx, Themed } from "theme-ui"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <Themed.h1>Hello world!</Themed.h1>
+      Go to{" "}
+      <Themed.a as={Link} to="/page-2">
+        page 2
+      </Themed.a>
+    </Layout>
+  )
 }
