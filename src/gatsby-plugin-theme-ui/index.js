23242dd29b9e708b8e94e5a8e68a6ea6953b4ef5
Rodrigo Moreira <hola@piducan.dev>
2021-05-08 20:11:36 -0400

    update theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,49 +1,45 @@
+import { tosh as light } from "@theme-ui/presets"
+import { merge } from "theme-ui"
+
 import "@fontsource/crimson-pro"
 import "@fontsource/crimson-pro/600.css"
 
-// example base theme from @theme-ui/presets
-const theme = {
-  breakpoints: ["40em", "52em", "64em"],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+const theme = merge(light, {
+  colors: {
+    secondary: "#FFD400",
+  },
+  sizes: {
+    container: 640,
+  },
   fonts: {
     body: "Crimson Pro",
     heading: "Crimson Pro",
-    monospace: "Menlo, monospace",
   },
-  fontSizes: [20, 24, 32, 40, 56, 80],
+  fontSizes: [14, 18, 20, 24, 32, 48, 64, 72],
   fontWeights: {
     body: 400,
     heading: 600,
     bold: 600,
   },
-  lineHeights: {
-    body: 1.5,
-    heading: 1.125,
-  },
-  colors: {
-    text: "#000",
-    background: "#fff",
-    primary: "#07c",
-    secondary: "#30c",
-    muted: "#f6f6f6",
-  },
-  sizes: {
-    container: 640,
-  },
-  text: {
-    heading: {
-      fontFamily: "heading",
-      lineHeight: "heading",
-      fontWeight: "heading",
+  buttons: {
+    primary: {
+      border: theme => `1px solid ${theme.colors.primary}`,
+      cursor: "pointer",
+      bg: "primary",
+      color: "background",
+      "&:hover": {
+        bg: "secondary",
+        color: "primary",
+      },
     },
   },
   styles: {
-    root: {
-      fontFamily: "body",
-      lineHeight: "body",
-      fontWeight: "body",
+    p: {
+      textAlign: "justify",
+      // textIndent: "1.6em",
     },
     a: {
+      fontWeight: "heading",
       color: "primary",
       textDecoration: "none",
       "&:hover": {
@@ -51,60 +47,7 @@ const theme = {
       },
       cursor: "pointer",
     },
-    h1: {
-      variant: "text.heading",
-      fontSize: 5,
-    },
-    h2: {
-      variant: "text.heading",
-      fontSize: 4,
-    },
-    h3: {
-      variant: "text.heading",
-      fontSize: 3,
-    },
-    h4: {
-      variant: "text.heading",
-      fontSize: 2,
-    },
-    h5: {
-      variant: "text.heading",
-      fontSize: 1,
-    },
-    h6: {
-      variant: "text.heading",
-      fontSize: 0,
-    },
-    p: {
-      fontSize: 0,
-      textAlign: "justify",
-      textIndent: "1.8em",
-    },
-    pre: {
-      fontFamily: "monospace",
-      overflowX: "auto",
-      code: {
-        color: "inherit",
-      },
-    },
-    code: {
-      fontFamily: "monospace",
-      fontSize: "inherit",
-    },
-    table: {
-      width: "100%",
-      borderCollapse: "separate",
-      borderSpacing: 0,
-    },
-    th: {
-      textAlign: "left",
-      borderBottomStyle: "solid",
-    },
-    td: {
-      textAlign: "left",
-      borderBottomStyle: "solid",
-    },
   },
-}
+})
 
 export default theme
